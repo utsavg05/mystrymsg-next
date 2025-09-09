96fee028b8efe2d27cc3dd1003c3d42ad0e8a663
utsavg05 <utsav.gp1204@gmail.com>
2025-09-09 22:36:51 +0530

    feat: add initial schemas and components for user messaging system
    - Created `acceptMessageSchema.ts`, `messageSchema.ts`, `signInSchema.ts`, `signUpSchema.ts`, and `verifySchema.ts` for handling message and user data.
    - Updated `package.json` and `package-lock.json` to include new dependencies: `@radix-ui/react-slot`, `class-variance-authority`, `clsx`, `lucide-react`, `mongoose`, and `tailwind-merge`.
    - Added `globals.css` for global styles and theme management.
    - Implemented `Button` component in `button.tsx` with variants and sizes.
    - Created utility function `cn` in `utils.ts` for merging class names.
    - Defined `User` and `Message` models in `User.ts` for MongoDB integration.

---
+++ src/model/User.ts
@@ -0,0 +1,72 @@
+import mongoose, {Schema, Document} from 'mongoose'
+
+export interface Message extends Document {
+    content: string,
+    CreatedAt: Date
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    CreatedAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    isVerified: boolean;
+    verifyCodeExpiry: Date;
+    isMessageAccepting: boolean;
+    messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "username is required"],
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "username is required"],
+        unique: true,
+        match: [/.+\@.+\..+/, "Please use a valid email address"]
+
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"], 
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify Code is required"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify Code Expiry is required"],
+    },
+    isMessageAccepting: {
+        type: Boolean,
+        default: true,
+    },
+    messages: [MessageSchema]
+})
+
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema) 
+
+export default UserModel;
