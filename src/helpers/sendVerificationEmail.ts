ec8e972730523980953d2888f5ab524e7a0e916f
utsavg05 <utsav.gp1204@gmail.com>
2025-09-10 19:05:25 +0530

    api for user sign-up

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,22 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'Mystry Message | Verification code',
+            react: VerificationEmail({ username, otp: verifyCode }),
+        });
+        return { success: true, message: 'Verification email successfully sent' }
+    } catch (emailError) {
+        console.log('error sending email', emailError)
+        return { success: false, message: 'Failed to send verification email' }
+    }
+}
