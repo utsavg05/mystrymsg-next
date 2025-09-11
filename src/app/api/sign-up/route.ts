4d148443094aab27c51dd021fa72ed18ef8aff2a
utsavg05 <utsav.gp1204@gmail.com>
2025-09-11 18:40:31 +0530

    feat: add next-auth integration for authentication and user verification

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,7 +2,6 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from 'bcryptjs'
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
-import { success } from "zod";
 
 
 export async function POST(request: Request) {
