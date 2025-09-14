9e36527aa36869b14696261b94265bb8b5598bad
utsavg05 <utsav.gp1204@gmail.com>
2025-09-14 18:17:04 +0530

    send email verified

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
     try {
         await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
+            from: 'utsavgupta31122004@gmail.com',
             to: email,
             subject: 'Mystry Message | Verification code',
             react: VerificationEmail({ username, otp: verifyCode }),
